7c56540123a69887afdd30dfc69fc1493434cd9b
kendrafarinas <kendraviera@hotmail.com>
2019-09-28 11:56:17 -0400

    Primer Commit

---
+++ Brito/Brito/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Brito.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Brito/Brito/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Brito.WebApiApplication" Language="C#" %>
